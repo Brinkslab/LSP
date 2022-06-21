aaabbd1a6f0b26c5ef9a459ba5e89de368f19287
Brinkslab <61872159+Brinkslab@users.noreply.github.com>
2022-06-21 16:14:29 +0200

    Update field_enhancement_plot.py

--- field_enhancement_plot.py
+++ field_enhancement_plot.py
@@ -1,19 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec  3 11:09:11 2021
-
-@author: LocalAdmin
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
+from matplotlib import rc
 from cmcrameri import cm
 
 folder="..."
 file_name="SIM_lambda540nm_XY"
 file_path=folder+file_name+".txt"
 pixels=401
+
+font = {'family' : 'normal',
+        'weight' : 'normal',
+        'size'   : 14}
+
+rc('font', **font)
 
 data=np.loadtxt(file_path,dtype=str,delimiter="\n")
 
@@ -31,10 +31,7 @@ E=E.transpose()
 plt.figure()
 plt.imshow(E, interpolation='antialiased',cmap=cm.batlow,extent=[x[0]*1e9,x[-1]*1e9,y[0]*1e9,y[-1]*1e9],norm=colors.LogNorm())
 cbar=plt.colorbar()
-cbar.set_label(label='Field enhancement [ |E|\u00b2 / |E\u2070|\u00b2 ]',rotation=270, labelpad=15)
-plt.xlabel('x [nm]')
-plt.ylabel('y [nm]')
-plt.xticks(np.linspace(-50,50,11),fontsize=9)
-plt.yticks(np.linspace(-50,50,11),fontsize=9)
+cbar.set_label(label='Field enhancement [ |E|\u00b2 / |E\u2070|\u00b2 ]',rotation=270, labelpad=20)
+plt.axis('off')
 plt.savefig(folder+"/plots/"+file_name+'map.png',dpi=300)
 plt.show()
